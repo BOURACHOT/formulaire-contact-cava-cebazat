c5ac528fe05dfb0804712c2670db5a6127d2d2bd
Bojulot <joel.bourachot@orange.fr>
2019-02-11 21:43:48 +0100

    mise en forme => formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,10 +1,65 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Document sans titre</title>
-</head>
+<?php
+/* Remplacer votre_adresse@mail.net par votre adresse mail
+   Remplacer www.votre_domaine.net par votre nom de domaine */
 
-<body>
-</body>
-</html>
+$adresse = "contact@cava-cebazat.fr";
+$site = "cava-cebazat.fr";
+
+$TO = $adresse;
+
+$head = "From: ".$adresse."\n";
+$head .= "X-Sender: <".$adresse.">\n";
+$head .= "X-Mailer: PHP\n";
+$head .= "Return-Path: <".$adresse.">\n";
+$head .= "Content-Type: text/plain; charset=iso-8859-1\n";
+
+$sujet = "Formulaire de contact";
+
+$informations = "
+Nom: ".$_POST['realname']." \r\n
+Email du formulaire: ".$_POST['email']." \r\n
+Sujet du formulaire: ".$_POST['title']."\r\n
+Message: ".$_POST['comments']." \r\n
+";
+
+
+
+
+
+
+// debut mise en place recaptcha
+
+    	
+	// Ma clé privée
+	$secret = "6Ld9WI0UAAAAAIhjiWBq6dTSaarhexGZVXGBajhz";
+	// Paramètre renvoyé par le recaptcha
+	$response = $_POST['g-recaptcha-response'];
+	// On récupère l'IP de l'utilisateur
+	$remoteip = $_SERVER['REMOTE_ADDR'];
+	
+	$api_url = "https://www.google.com/recaptcha/api/siteverify?secret=" 
+	    . $secret
+	    . "&response=" . $response
+	    . "&remoteip=" . $remoteip ;
+	
+	$decode = json_decode(file_get_contents($api_url), true);
+	
+	if ($decode['success'] == true) {
+		// C'est un humain
+		$res = mail($TO, $sujet ,$informations, $head);
+		Header("Location: http://".$site."/html/formail2_ok.html" );
+	}
+	
+	else {
+		// C'est un robot ou le code de vérification est incorrecte
+		Header("Location: http://".$site."/html/formail2_pasok.html" );
+	}
+		
+
+
+
+//  fin de mise en place recaptcha
+?>
+
+
+
